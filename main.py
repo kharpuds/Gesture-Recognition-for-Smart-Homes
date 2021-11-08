3e208135ae77f0d98ecf6b32fc6d3135489dc6e1
Simran Kharpude <66372058+kharpuds@users.noreply.github.com>
2021-11-08 13:53:09 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,93 @@
+import cv2
+import numpy as np
+import os
+import tensorflow as tf
+import os
+import handshape_feature_extractor 
+from handshape_feature_extractor import HandShapeFeatureExtractor
+from frameextractor import frameExtractor
+from scipy import spatial
+import csv
+from csv import reader
+
+middle_frames = []
+extractor = HandShapeFeatureExtractor().get_instance()
+
+## import the handfeature extractor class
+
+# =============================================================================
+# Get the penultimate layer for trainig data
+# =============================================================================
+# your code goes here
+# Extract the middle frame of each gesture video
+
+gnames = sorted(os.listdir("traindata/"))
+outputs = []
+c=0
+
+with open('training_data_frames_features.csv') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	line_count = 0
+	for row in csv_reader:
+		middle_frames.append([float(x) for x in row])
+
+with open('training_data_output_labels.csv') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	line_count = 0
+	for row in csv_reader:
+		outputs.append(int(row[0]))
+
+
+for gname in gnames:
+	path = os.path.join("traindata",gname)
+	frames = frameExtractor(path,-1)
+	for frame in frames:
+		fvect=[]
+		frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+		frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		fvect.extend(np.squeeze(extractor.extract_feature(frame)))
+		middle_frames.append(fvect)
+		outputs.append(c%17)
+	c+=1
+
+# =============================================================================
+# Get the penultimate layer for test data
+# =============================================================================
+# your code goes here 
+# Extract the middle frame of each gesture video
+
+results = []
+gnames = sorted(os.listdir("test/"))
+c=0
+
+for gname in gnames:
+	path = os.path.join("test",gname)
+	frames = frameExtractor(path,-1)
+	fvect=[]
+	for frame in frames:
+		frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+		frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		fvect.extend(np.squeeze(extractor.extract_feature(frame)))
+	cc=0
+	mindisti, mindist = 0,1e8
+	for v in middle_frames:
+		cosdist = spatial.distance.cosine(fvect,v)
+		if cosdist<mindist:
+			mindist = cosdist
+			mindisti = cc
+		cc+=1
+	print(gname, outputs[mindisti])
+	results.append([ outputs[mindisti] ])
+	c+=1
+
+
+# =============================================================================
+# Recognize the gesture (use cosine similarity for comparing the vectors)
+# =============================================================================
+
+filename = "Results.csv"
+with open(filename, 'w') as csvfile: 
+	csvwriter = csv.writer(csvfile)
+	csvwriter.writerows(results)
---
+++ main_train.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 00:44:25 2021
+
+@author: chakati
+"""
+
+import cv2
+import csv
+import numpy as np
+import os
+from frameextractor import frameExtractor
+# import handshape_feature_extractor 
+from handshape_feature_extractor import HandShapeFeatureExtractor
+
+traindata_middle_frames = []
+outputs = []
+
+gestureLabel = {}
+for x in range(10):
+	gestureLabel["Num"+str(x)] = x
+gestureLabel["FanDown"] = 10
+gestureLabel["FanOn"] = 11
+gestureLabel["FanOff"] = 12
+gestureLabel["FanUp"] = 13
+gestureLabel["LightOff"] = 14
+gestureLabel["LightOn"] = 15
+gestureLabel["SetThermo"] = 16
+
+# test_middle_frames = []
+# test_outputs = []
+
+# def adjust_gamma(image, gamma=1.0):
+#    invGamma = 1.0 / gamma
+#    table = np.array([((i / 255.0) ** invGamma) * 255
+#       for i in np.arange(0, 256)]).astype("uint8")
+#    return cv2.LUT(image, table)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+hfe = HandShapeFeatureExtractor()
+# print(extractor.model.summary())
+
+gestures = os.listdir("traindata")
+for g in gestures:
+	print(g)
+	gesture_label = os.listdir("traindata\\"+g)
+	# for x in range(2):
+	for label in gesture_label:
+		path = "traindata\\"+g+"\\"+label #str(x)+"_Venkobarao.mp4"
+		frames = frameExtractor(path,-1)
+		for frame in frames:
+			fvect=[]
+			frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+			frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+			# frame = adjust_gamma(frame, gamma=0.45)
+			frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			fvect.extend(np.squeeze(hfe.extract_feature(frame)))
+			traindata_middle_frames.append(np.array(fvect))
+			outputs.append([gestureLabel[g]])
+		print(len(frames), len(fvect))
+	# print("=====")
+
+traindata_middle_frames = np.array(traindata_middle_frames)
+outputs = np.array(outputs)
+filename = "training_data_frames_features.csv"
+with open(filename, 'w') as csvfile: 
+	csvwriter = csv.writer(csvfile)
+	csvwriter.writerows(traindata_middle_frames)
+
+filename = "training_data_output_labels.csv"
+with open(filename, 'w') as csvfile: 
+	csvwriter = csv.writer(csvfile)
+	csvwriter.writerows(outputs)
+
+print(traindata_middle_frames.shape, outputs.shape)
+
+
+# import cv2
+# import csv
+# import numpy as np
+# import os
+# from frameextractor import frameExtractor
+# import handshape_feature_extractor 
+# from handshape_feature_extractor import HandShapeFeatureExtractor
+
+# middle_frames = []
+# outputs = []
+
+# gestID = {}
+# for x in range(10):
+# 	gestID["Num"+str(x)] = x
+# gestID["FanDown"] = 10
+# gestID["FanOn"] = 11
+# gestID["FanOff"] = 12
+# gestID["FanUp"] = 13
+# gestID["LightOff"] = 14
+# gestID["LightOn"] = 15
+# gestID["SetThermo"] = 16
+
+# test_middle_frames = []
+# test_outputs = []
+
+# # def adjust_gamma(image, gamma=1.0):
+# #    invGamma = 1.0 / gamma
+# #    table = np.array([((i / 255.0) ** invGamma) * 255
+# #       for i in np.arange(0, 256)]).astype("uint8")
+# #    return cv2.LUT(image, table)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# extractor = HandShapeFeatureExtractor()
+# # print(extractor.model.summary())
+
+# gnames = os.listdir("traindata")
+# for gname in gnames:
+# 	print(gname)
+# 	gnameids = os.listdir("traindata\\"+gname)
+# 	# for x in range(2):
+# 	for gnameid in gnameids:
+# 		path = "traindata\\"+gname+"\\"+gnameid #str(x)+"_Venkobarao.mp4"
+# 		frames = frameExtractor(path,-1)
+# 		for frame in frames:
+# 			fvect=[]
+# 			frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+# 			frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+# 			# frame = adjust_gamma(frame, gamma=0.45)
+# 			frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# 			fvect.extend(np.squeeze(extractor.extract_feature(frame)))
+# 			middle_frames.append(np.array(fvect))
+# 			outputs.append([gestID[gname]])
+# 		print(len(frames), len(fvect))
+# 	# print("=====")
+
+# middle_frames = np.array(middle_frames)
+# outputs = np.array(outputs)
+# filename = "train_frames.csv"
+# with open(filename, 'w') as csvfile: 
+# 	csvwriter = csv.writer(csvfile)
+# 	csvwriter.writerows(middle_frames)
+
+# filename = "train_outputs.csv"
+# with open(filename, 'w') as csvfile: 
+# 	csvwriter = csv.writer(csvfile)
+# 	csvwriter.writerows(outputs)
+
+# print(middle_frames.shape, outputs.shape)
